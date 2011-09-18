c964df9680184f057e8bcce1a9b1d7306be09472
Christian Hein <chrishein@gmail.com>
2011-09-18 01:15:03 -0300

    Created basic unit tests

--- test/general_test.rb
+++ test/general_test.rb
@@ -1,21 +1,30 @@
 # coding: utf-8
-require '../src/bing_translate.rb'
+require '../lib/bing_translate.rb'
+require "test/unit"
 
-from = 'es'
-to = 'en'
-@bt = Bing::Translator.new(from, to, {:app_id => ENV['BING_APP_ID']})
-puts @bt.translate('perro')
+class TestBingTranslate < Test::Unit::TestCase
+  
+  def test_spanish
+    from = 'es'
+    to = 'en'
+    @bt = Bing::Translator.new(from, to, {:app_id => ENV['BING_APP_ID']})
+    
+    assert_equal('dog', @bt.translate('perro') )
+    assert_equal('Dog', @bt.translate('Perro') )
+    assert_equal('Hello', @bt.translate('Hola') )
+    assert_equal('Athletic', @bt.translate('Atlético') )
+    assert_equal('Giraffe', @bt.translate('Jirafa') )
+    assert_equal('search', @bt.translate('búsqueda') )
+    assert_equal('translation', @bt.translate('traducción') )
+  end
+    
+  def test_french
+    from = 'es'
+    to = 'fr'
+    @bt = Bing::Translator.new(from, to, {:app_id => ENV['BING_APP_ID']})
 
-terms = ['Perro', 'Gato', 'Hola', 'Atlético', 'Perro', 'Jirafa', 'búsqueda', 'traducción', 'gato', 'perro']
+    assert_equal('chien', @bt.translate('perro') )
+    
+  end
 
-terms.each do |t|
-  puts @bt.translate(t)
-end
-
-
-to = 'fr'
-@bt = Bing::Translator.new(from, to, {:app_id => ENV['BING_APP_ID']})
-
-terms.each do |t|
-  puts @bt.translate(t)
 end
